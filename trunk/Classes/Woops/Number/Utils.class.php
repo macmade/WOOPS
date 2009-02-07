1312028bf02322cb8e96055fe4c769db419df3a6
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-07 05:56:16 +0000

    Added the number utilities class.
    git-svn-id: https://woops.googlecode.com/svn@13 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/Classes/Woops/Number/Utils.class.php
@@ -0,0 +1,70 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Number related utilities
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops/Number
+ */
+class tx_oop_Number_Utils
+{
+    /**
+     * Class constructor
+     * 
+     * The class constructor is private as all methods from this class are
+     * static.
+     * 
+     * @return  NULL
+     */
+    private function __construct()
+    {}
+    
+    /**
+     * Ensures a number is in a specified range
+     * 
+     * This method forces the specified number into the boundaries of a
+     * minimum and maximum number.
+     * 
+     * @param   numbe   The number to check
+     * @param   number  The minimum value
+     * @param   number  The maximum value
+     * @param   boolean Evaluates the number as an integer
+     * @return  number  A number in the specified range
+     */
+    public static function inRange( $number, $min, $max, $int = false )
+    {
+        // Checks if we must evaluate the number as an integer
+        if( $int ) {
+            
+            // Converts the number to an integer
+            $number = ( int )$number;
+        }
+        
+        // Checks the number
+        if( $number > $max ) {
+            
+            // Number is bigger than maximum value
+            $number = $max;
+            
+        } elseif( $number < $min ) {
+            
+            // Number is smaller than minimal value
+            $number = $min;
+        }
+        
+        // Returns the number
+        return $number;
+    }
+}
