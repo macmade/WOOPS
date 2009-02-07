a080d79a59bba5c2b58657edfe5796fc4c15ae41
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-07 08:32:15 +0000

    Added the exception handler class.
    git-svn-id: https://woops.googlecode.com/svn@38 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Core/Exception/Handler.class.php
@@ -0,0 +1,57 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * WOOPS exception handler class
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Core.Exception
+ */
+final class Woops_Core_Exception_Handler
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * Class constructor
+     * 
+     * The class constructor is private as all methods from this class are
+     * static.
+     * 
+     * @return  NULL
+     */
+    private function __construct()
+    {}
+    
+    /**
+     * 
+     */
+    public static function handleException( Exception $e )
+    {
+        if( is_subclass_of( $e, 'Woops_Core_Exception_Base' ) ) {
+            
+            print $e->getInfos();
+            
+        } else {
+            
+            $e = new Woops_Core_Php_Exception(
+                'Exception of type ' . get_class( $e ) . ': ' . $e->getMessage(),
+                $e->getCode(), $e->getTrace()
+            );
+            print $e->getInfos();
+        }
+    }
+}
