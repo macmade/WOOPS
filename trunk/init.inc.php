b0f7c6b5e978318bfb0af64f655a9b875e410ba0
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-07 04:52:54 +0000

    Added a basic initialization script.
    git-svn-id: https://woops.googlecode.com/svn@3 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/init.inc.php
+++ trunk/init.inc.php
@@ -1,1 +1,61 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
 
+# $Id$
+
+// Sets the error reporting level to the highest possible value
+error_reporting( E_ALL | E_STRICT );
+
+// Checks the PHP version
+if( ( double )PHP_VERSION < 5.2 ) {
+    
+    // We are not running PHP 5.2 or greater
+    trigger_error(
+        'PHP version 5.2 is required to use this script (actual version is ' . PHP_VERSION . ')',
+        E_USER_ERROR
+    );
+}
+
+// Checks for the SPL
+if( !function_exists( 'spl_autoload_register' ) ) {
+    
+    // The SPL is unavailable
+    throw new Exception(
+        'The SPL (Standard PHP Library) is required to use this script'
+    );
+}
+
+// Checks for the SimpleXMLElement class
+if( !class_exists( 'SimpleXMLElement' ) ) {
+    
+    // SimpleXMLElement is unavailable
+    throw new Exception(
+        'The SimpleXMLElement class is required to use this script'
+    );
+}
+
+// Includes the Woops class manager
+require_once(
+    dirname( __FILE__ )
+  . DIRECTORY_SEPARATOR
+  . 'Classes'
+  . DIRECTORY_SEPARATOR
+  . 'Woops'
+  . DIRECTORY_SEPARATOR
+  . 'Core'
+  . DIRECTORY_SEPARATOR
+  . 'Class'
+  . DIRECTORY_SEPARATOR
+  . 'Manager.class.php'
+);
+
+// Registers an SPL autoload method to use to load the classes form the Woops project
+spl_autoload_register( array( 'Woops_Core_Class_Manager', 'autoLoad' ) );
