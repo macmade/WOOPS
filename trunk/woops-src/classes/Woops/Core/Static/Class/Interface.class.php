3c3076fbd4d0943739be227a94116bf2a4a7f363
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-07 19:13:49 +0000

    Added an interface for the static classes.
    git-svn-id: https://woops.googlecode.com/svn@57 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Core/Static/Class/Interface.class.php
@@ -0,0 +1,22 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Interface for the static classes
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Core.Static.Class
+ */
+interface Woops_Core_Static_Class_Interface
+{}
