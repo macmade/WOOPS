62d1979067ae6f3f40661632f1672254467b0d8e
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-07 19:18:45 +0000

    Removed the getInstance() method declaration in the singleton interface, as it will prevent singleton classes to have arguments in the constructor.
    git-svn-id: https://woops.googlecode.com/svn@60 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Core/Singleton/Interface.class.php
+++ trunk/woops-src/classes/Woops/Core/Singleton/Interface.class.php
@@ -19,9 +19,4 @@
  * @package     Woops.Core.Singleton
  */
 interface Woops_Core_Singleton_Interface
-{
-    /**
-     * The method used to get the instance of a singleton class
-     */
-    static function getInstance();
-}
+{}
