ffd0439824962abd1aa134def9760269d757e4ee
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-07 05:42:01 +0000

    Added an interface for the singleton classes.
    git-svn-id: https://woops.googlecode.com/svn@10 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/Classes/Woops/Core/Singleton/Interface.class.php
@@ -0,0 +1,27 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Interface for the singleton classes
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops/Core/Singleton
+ */
+interface Woops_Core_Singleton_Interface
+{
+    /**
+     * The method used to get the instance of a singleton class
+     */
+    static function getInstance();
+}
