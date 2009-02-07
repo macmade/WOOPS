21a3bafd832b4f7b51c36f66a866680a717fa7e0
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-07 05:06:02 +0000

    The index page now includes the initialization script.
    git-svn-id: https://woops.googlecode.com/svn@7 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/index.php
+++ trunk/index.php
@@ -1,1 +1,15 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
 
+# $Id$
+
+// Includes the initialization script
+require_once( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'init.inc.php' );
