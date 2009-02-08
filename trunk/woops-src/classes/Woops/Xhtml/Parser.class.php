5c17cbba2cbf0dabe0948a25df570126dda6e7c0
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-08 22:52:49 +0000

    Added the placeholder for the XHTML parser class.
    git-svn-id: https://woops.googlecode.com/svn@89 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Xhtml/Parser.class.php
@@ -0,0 +1,27 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * XHTML parser class
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Xhtml
+ */
+class Woops_Xhtml_Parser
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+}
