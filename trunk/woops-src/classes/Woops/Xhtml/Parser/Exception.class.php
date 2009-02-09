c5fec6f227d10d4f33cb02984ce037444ff9c28b
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-09 01:26:22 +0000

    Added the XHTML parser.
    git-svn-id: https://woops.googlecode.com/svn@98 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Xhtml/Parser/Exception.class.php
@@ -0,0 +1,35 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Exception class for the Woops_Xhtml_Parser class
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Soap.Server
+ */
+final class Woops_Xhtml_Parser_Exception extends Woops_Core_Exception_Base
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * Error codes for the exceptions
+     */
+    const EXCEPTION_NO_FILE           = 0x01;
+    const EXCEPTION_FILE_NOT_READABLE = 0x02;
+    const EXCEPTION_INVALID_CHARSET   = 0x03;
+    const EXCEPTION_XML_PARSER_ERROR  = 0x04;
+}
