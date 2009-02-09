3e9cf008e5970904a930182605344dc1e4ce7af5
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-09 17:48:50 +0000

    The configuration file is now an INI file.
    git-svn-id: https://woops.googlecode.com/svn@129 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/config.ini.php
@@ -0,0 +1,88 @@
+; WOOPS configuration file <?php exit(); ?>
+
+; ##############################################################################
+; #                                                                            #
+; #               WOOPS - Web Object Oriented Programming System               #
+; #                                                                            #
+; #                              COPYRIGHT NOTICE                              #
+; #                                                                            #
+; # (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)             #
+; # All rights reserved                                                        #
+; ##############################################################################
+
+;  $Id$
+
+; ##############################################################################
+; # Database settings                                                          #
+; ##############################################################################
+
+[database]
+
+; The database driver to use, with PDO
+driver      = mysql
+
+; The database host
+host        = localhost
+
+; The database port
+port        = 3306
+
+; The database username
+user        = woops
+
+; The database user password
+password    = woops
+
+; The name of the database to use
+name        = woops
+
+; The prefix to use for all WOOPS tables
+tablePrefix = WOOPS_
+
+; ##############################################################################
+; # Date and time settings                                                     #
+; ##############################################################################
+
+[time]
+
+; The default timezone
+timezone = Europe/Zurich
+
+; ##############################################################################
+; # Language settings                                                          #
+; ##############################################################################
+
+[lang]
+
+; The default language
+defaultLanguage = en
+
+; ##############################################################################
+; # XHTML settings                                                             #
+; ##############################################################################
+
+[xhtml]
+
+; Format and indent the generated XHTML code
+format               = On
+
+; The output charset
+charset              = utf-8
+
+; The XHTML document type
+doctype              = xhtml1-strict
+
+; Inserts the XML declaration
+insertXmlDeclaration = On
+
+; Inserts the XHTML document type
+insertDoctype        = On
+
+; ##############################################################################
+; # Error related settings                                                     #
+; ##############################################################################
+
+[error]
+
+; The error report type (development - production - none)
+report = development
