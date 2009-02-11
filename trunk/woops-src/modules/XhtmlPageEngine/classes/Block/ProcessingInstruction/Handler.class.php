fc72db0f8d8ebfb3789c9c553e03abf6e090660d
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-11 05:35:14 +0000

    Added the placeholder for the PI handler.
    git-svn-id: https://woops.googlecode.com/svn@175 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/modules/XhtmlPageEngine/classes/Block/ProcessingInstruction/Handler.class.php
@@ -0,0 +1,33 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Handler for the 'woops-block-module' XHTML processing instructions
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Mod.XhtmlPageEngine.Block.ProcessingInstruction
+ */
+class Woops_Mod_XhtmlPageEngine_Block_ProcessingInstruction_Handler implements Woops_Xhtml_ProcessingInstruction_Handler_Interface
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * 
+     */
+    function process( stdClass $options )
+    {}
+}
