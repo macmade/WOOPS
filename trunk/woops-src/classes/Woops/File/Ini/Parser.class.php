9c522ea00849428794e66eddf414f785dd02cd14
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-13 16:35:09 +0000

    The INI file parser is now able to basically parse an INI file.
    git-svn-id: https://woops.googlecode.com/svn@271 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/File/Ini/Parser.class.php
+++ trunk/woops-src/classes/Woops/File/Ini/Parser.class.php
@@ -36,14 +36,19 @@ class Woops_File_Ini_Parser
     private static $_str       = NULL;
     
     /**
-     * Class constructor
      * 
-     * The class constructor is private as all methods from this class are
-     * static.
-     * 
-     * @return  NULL
      */
-    private function __construct( $filePath )
+    protected $_filePath       = '';
+    
+    /**
+     * 
+     */
+    protected $_values         = array();
+    
+    /**
+     * 
+     */
+    public function __construct( $path )
     {
         // Checks if the static variables are set
         if( !self::$_hasStatic ) {
@@ -51,6 +56,26 @@ class Woops_File_Ini_Parser
             // Sets the static variables
             self::_setStaticVars();
         }
+        
+        if( !file_exists( $path ) ) {
+            
+            throw new Woops_File_Ini_Parser_Exception(
+                '',
+                Woops_File_Ini_Parser_Exception::EXCEPTION_NO_FILE
+            );
+        }
+        
+        if( !is_readable( $path ) ) {
+            
+            throw new Woops_File_Ini_Parser_Exception(
+                '',
+                Woops_File_Ini_Parser_Exception::EXCEPTION_FILE_NOT_READABLE
+            );
+        }
+        
+        $this->_filePath = $path;
+        
+        $this->_parseFile();
     }
     
     /**
@@ -65,5 +90,109 @@ class Woops_File_Ini_Parser
         
         // Static variables are set
         self::$_hasStatic = true;
+    }
+    
+    /**
+     * 
+     */
+    protected function _parseFile()
+    {
+        $lines    = file( $this->_filePath );
+        $section  = '';
+        $comments = array();
+        
+        foreach( $lines as &$line ) {
+            
+            $matches = array();
+            
+            if( !trim( $line ) ) {
+                
+                $comments = array();
+            }
+            
+            if( preg_match( '/^\s*;\s*(.*)$/', $line, $matches ) ) {
+                
+                $comments[] = trim( $matches[ 1 ] );
+            }
+            
+            if( preg_match( '/^\s*\[([^\]]+)\]/', $line, $matches ) ) {
+                
+                $section = $matches[ 1 ];
+                $this->_values[ $section ] = array();
+                continue;
+            }
+            
+            if( preg_match( '/^\s*([^;=\s]+)\s*=\s+([^;\s]+)/', $line, $matches ) ) {
+                
+                $key   = $matches[ 1 ];
+                $value = $matches[ 2 ];
+                
+                if( $value === 'Off' ) {
+                    
+                    $value = false;
+                    
+                } elseif( $value === 'On' ) {
+                    
+                    $value = true;
+                }
+                
+                if( $section ) {
+                    
+                    if( substr( $key, -2 ) === '[]' ) {
+                        
+                        $key = substr( $key, 0, -2 );
+                        
+                        if( !isset( $this->_values[ $section ][ $key ] ) ) {
+                            
+                            $this->_values[ $section ][ $key ] = array(
+                                'value'    => array(),
+                                'comments' => $comments
+                            );
+                            
+                            $comments = array();
+                        }
+                        
+                        $this->_values[ $section ][ $key ][ 0 ][] = $value;
+                        
+                    } else {
+                        
+                        $this->_values[ $section ][ $key ] = array(
+                            'value'    => $value,
+                            'comments' => $comments
+                        );
+                        
+                        $comments = array();
+                    }
+                    
+                } else {
+                    
+                    if( substr( $key, -2 ) === '[]' ) {
+                        
+                        $key = substr( $key, 0, -2 );
+                        
+                        if( !isset( $this->_values[ $key ] ) ) {
+                            
+                            $this->_values[ $key ] = array(
+                                'value'    => array(),
+                                'comments' => $comments
+                            );
+                            
+                            $comments = array();
+                        }
+                        
+                        $this->_values[ $key ][ 0 ][] = $value;
+                        
+                    } else {
+                        
+                        $this->_values[ $key ] = array(
+                            'value'    => $value,
+                            'comments' => $comments
+                        );
+                        
+                        $comments = array();
+                    }
+                }
+            }
+        }
     }
 }
