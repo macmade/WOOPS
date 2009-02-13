1bb83c9a89791a4d7c196c71591bc614056d2ec8
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-13 03:43:29 +0000

    Added a class that contains the WOOPS version.
    git-svn-id: https://woops.googlecode.com/svn@233 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Core/Informations.class.php
@@ -0,0 +1,43 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Class that contains WOOPS information constants
+ * 
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Core
+ */
+final class Woops_Core_Informations
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The WOOPS version
+     */
+    const VERSION        = '0.0.0';
+    
+    /**
+     * Class constructor
+     * 
+     * The class constructor is private as this class should never been
+     * instanciated (it doesn't contains any methods).
+     * 
+     * @return  NULL
+     */
+    private function __construct()
+    {}
+}
