5d5bb77d9082c78452f9a92cd60f3128a63e8901
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-13 11:24:11 +0000

    Added the placeholder for the INI file parser class.
    git-svn-id: https://woops.googlecode.com/svn@254 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/File/Ini/Parser.class.php
@@ -0,0 +1,69 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * INI file parser
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.File.Ini
+ */
+class Woops_File_Ini_Parser
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * Wether the static variables are set or not
+     */
+    private static $_hasStatic = false;
+    
+    /**
+     * The string utilities
+     */
+    private static $_str       = NULL;
+    
+    /**
+     * Class constructor
+     * 
+     * The class constructor is private as all methods from this class are
+     * static.
+     * 
+     * @return  NULL
+     */
+    private function __construct( $filePath )
+    {
+        // Checks if the static variables are set
+        if( !self::$_hasStatic ) {
+            
+            // Sets the static variables
+            self::_setStaticVars();
+        }
+    }
+    
+    /**
+     * Sets the needed static variables
+     * 
+     * @return  NULL
+     */
+    private static function _setStaticVars()
+    {
+        // Gets the instance of the string utilities
+        self::$_str       = Woops_String_Utils::getInstance();
+        
+        // Static variables are set
+        self::$_hasStatic = true;
+    }
+}
---
+++ trunk/woops-src/classes/Woops/File/Ini/Parser/Exception.class.php
@@ -0,0 +1,31 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Exception class for the Woops_File_Ini_Parser class
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.File.Ini.Parser
+ */
+final class Woops_File_Ini_Parser_Exception extends Woops_Core_Exception_Base
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * Error codes for the exceptions
+     */
+}
