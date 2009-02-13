6db5dbedc2128d75d4b064a389c4ccd1de06b141
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-13 14:37:06 +0000

    Added the base database files (structure and data).
    git-svn-id: https://woops.googlecode.com/svn@269 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/database/data.sql
@@ -0,0 +1,38 @@
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+#
+# Dumping data for table `WOOPS_CONTRIBUTORS`
+#
+INSERT INTO `WOOPS_CONTRIBUTORS` (`id_contributors`, `deleted`, `mtime`, `ctime`, `username`, `password`, `admin`, `email`, `fullname`, `lang`, `lastlogin`, `lastip`, `session`) VALUES
+(1, 0, 1230764400, 1230764400, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1, 'woops-admin@example.com', 'WOOPS administrator', 'en', 0, '', '');
+
+#
+# Dumping data for table `WOOPS_PAGEHEADERS`
+#
+INSERT INTO `WOOPS_PAGEHEADERS` (`id_pageheaders`, `id_pageinfos`, `id_templates`, `deleted`, `ctime`, `mtime`, `lang`, `title`, `menutitle`, `keywords`, `description`) VALUES
+(1, 1, 1, 0, 1230764400, 1230764400, 'en', 'WOOPS home page', '', '', ''),
+(2, 2, 2, 0, 1230764400, 1230764400, 'en', 'WOOPS administration page', '', '', '');
+
+#
+# Dumping data for table `WOOPS_PAGEINFOS`
+#
+INSERT INTO `WOOPS_PAGEINFOS` (`id_pageinfos`, `id_parent`, `deleted`, `ctime`, `mtime`, `home`) VALUES
+(1, 0, 0, 1230764400, 1230764400, 1),
+(2, 1, 0, 1230764400, 1230764400, 0);
+
+#
+# Dumping data for table `WOOPS_TEMPLATES`
+#
+INSERT INTO `WOOPS_TEMPLATES` (`id_templates`, `id_parent`, `deleted`, `ctime`, `mtime`, `title`, `file`, `engine`, `engine_options`) VALUES
+(1, 0, 0, 1230764400, 1230764400, 'WOOPS default template', 'woops-mod://Cms/ressources/templates/woops-default.html', 'Woops_Mod_Cms_Page_Engine', 'O:8:"stdClass":2:{s:6:"tagMap";a:0:{}s:8:"keepHead";a:1:{i:0;a:2:{i:0;s:4:"link";i:1;i:1;}}}'),
+(2, 0, 0, 1230764400, 1230764400, 'WOOPS administration template', 'woops-mod://Admin/ressources/templates/woops-admin.html', 'Woops_Mod_Cms_Page_Engine', 'O:8:"stdClass":2:{s:6:"tagMap";a:0:{}s:8:"keepHead";a:1:{i:0;a:2:{i:0;s:4:"link";i:1;i:1;}}}');
---
+++ trunk/woops-src/database/drop-tables.sql
@@ -0,0 +1,20 @@
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+#
+# Drops all database tables
+#
+DROP TABLE IF EXISTS `WOOPS_CONTRIBUTORS`;
+DROP TABLE IF EXISTS `WOOPS_CONTRIBUTORS_LOGS`;
+DROP TABLE IF EXISTS `WOOPS_PAGEHEADERS`;
+DROP TABLE IF EXISTS `WOOPS_PAGEINFOS`;
+DROP TABLE IF EXISTS `WOOPS_TEMPLATES`;
