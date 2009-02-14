b781e901e025300f31a7ef4e2618fc7e2b5c9637
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-14 07:40:25 +0000

    Added a version suffix.
    git-svn-id: https://woops.googlecode.com/svn@286 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Core/Informations.class.php
+++ trunk/woops-src/classes/Woops/Core/Informations.class.php
@@ -23,12 +23,17 @@ final class Woops_Core_Informations
     /**
      * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
      */
-    const PHP_COMPATIBLE = '5.2.0';
+    const PHP_COMPATIBLE       = '5.2.0';
     
     /**
      * The WOOPS version
      */
-    const WOOPS_VERSION  = '0.0.0';
+    const WOOPS_VERSION        = '0.0.0';
+    
+    /**
+     * The WOOPS version suffix (like rc, beta, aplha, dev, etc)
+     */
+    const WOOPS_VERSION_SUFFIX = 'dev';
     
     /**
      * Class constructor
