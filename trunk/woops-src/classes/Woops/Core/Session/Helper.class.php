4b6cb78e81b01ef2df3b14391b9111acc9620c26
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-18 16:56:44 +0000

    Added a base for a session class.
    git-svn-id: https://woops.googlecode.com/svn@342 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Core/Session/Helper.class.php
@@ -0,0 +1,162 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Session helper class
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Core.Session
+ */
+final class Woops_Core_Session_Helper implements Woops_Core_Singleton_Interface
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The session ID for the WOOPS
+     */
+    const SESSION_ID     = 'WOOPS-SESSION';
+    
+    /**
+     * The unique instance of the class (singleton)
+     */
+    private static $_instance = NULL;
+    
+    /**
+     * Wheter the WOOPS session is started
+     */
+    protected $_started     = '';
+    
+    /**
+     * Class constructor
+     * 
+     * The class constructor is private to avoid multiple instances of the
+     * class (singleton).
+     * 
+     * @return  void
+     */
+    private function __construct()
+    {}
+    
+    /**
+     * Clones an instance of the class
+     * 
+     * A call to this method will produce an exception, as the class cannot
+     * be cloned (singleton).
+     * 
+     * @return  void
+     * @throws  Woops_Core_Singleton_Exception  Always, as the class cannot be cloned (singleton)
+     */
+    public function __clone()
+    {
+        throw new Woops_Core_Singleton_Exception(
+            'Class ' . __CLASS__ . ' cannot be cloned',
+            Woops_Core_Singleton_Exception::EXCEPTION_CLONE
+        );
+    }
+    
+    /**
+     * Gets the unique class instance
+     * 
+     * This method is used to get the unique instance of the class
+     * (singleton). If no instance is available, it will create it.
+     * 
+     * @return  Woops_Core_Session_Helper   The unique instance of the class
+     * @see     __construct
+     */
+    public static function getInstance()
+    {
+        // Checks if the unique instance already exists
+        if( !is_object( self::$_instance ) ) {
+            
+            // Creates the unique instance
+            self::$_instance = new self();
+        }
+        
+        // Returns the unique instance
+        return self::$_instance;
+    }
+    
+    /**
+     * 
+     */
+    public function start()
+    {
+        if( !$this->_started ) {
+            
+            session_id( self::SESSION_ID );
+            session_start();
+            $this->_started = true;
+        }
+    }
+    
+    /**
+     * 
+     */
+    public function close()
+    {
+        if( $this->_started ) {
+            
+            session_write_close();
+            $this->_started = false;
+            $_SESSION = array();
+        }
+    }
+    
+    /**
+     * 
+     */
+    public function destroy()
+    {
+        if( $this->_started ) {
+            
+            session_destroy();
+            $this->_started = false;
+            $_SESSION = array();
+        }
+    }
+    
+    /**
+     * 
+     */
+    public function getSessionData( $key )
+    {
+        $this->start();
+        
+        if( isset( $_SESSION[ $key ] ) ) {
+            
+            return $_SESSION[ $key ];
+            
+        } else {
+            
+            return false;
+        }
+        
+        $this->close();
+    }
+    
+    /**
+     * 
+     */
+    public function setSessionData( $key, $value )
+    {
+        $this->start();
+        
+        $_SESSION[ $key ] = $value;
+        
+        $this->close();
+    }
+}
