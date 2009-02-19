d777b06d39ca2d9af0c5b4b5950bb1cf6d1508b8
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-19 11:30:51 +0000

    Added other unit classes placeholders.
    git-svn-id: https://woops.googlecode.com/svn@357 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Unit/Energy.class.php
@@ -0,0 +1,22 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Energy units
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Unit
+ */
+class Woops_Unit_Energy extends Woops_Unit_Base
+{}
---
+++ trunk/woops-src/classes/Woops/Unit/Work.class.php
@@ -0,0 +1,22 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Work units
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Unit
+ */
+class Woops_Unit_Work extends Woops_Unit_Energy
+{}
