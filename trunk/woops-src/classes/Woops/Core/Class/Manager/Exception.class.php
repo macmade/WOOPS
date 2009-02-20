5724852373ddbf55a1fd0b2d2e880c1b9626a574
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-20 23:33:51 +0000

    Added an exception class for the WOOPS class manager.
    git-svn-id: https://woops.googlecode.com/svn@372 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Core/Class/Manager/Exception.class.php
@@ -0,0 +1,33 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Exception class for the Woops_Core_Class_Manager class
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Core.Class.Manager
+ */
+final class Woops_Core_Class_Manager_Exception extends Woops_Core_Exception_Base
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * Error codes for the exceptions
+     */
+    const EXCEPTION_NOT_SINGLETON      = 0x01;
+    const EXCEPTION_NOT_MULTISINGLETON = 0x02;
+}
