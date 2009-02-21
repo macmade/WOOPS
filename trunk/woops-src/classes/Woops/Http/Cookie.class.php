f6630754d7dc8c0517a99f7f32244acd99be0d41
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-21 17:13:50 +0000

    Improved class comments.
    git-svn-id: https://woops.googlecode.com/svn@388 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Http/Cookie.class.php
+++ trunk/woops-src/classes/Woops/Http/Cookie.class.php
@@ -36,7 +36,7 @@ class Woops_Http_Cookie
     protected $_value    = '';
     
     /**
-     * The cookie's expiration date
+     * The cookie's expiration date (timestamp)
      */
     protected $_expires  = 0;
     
@@ -63,6 +63,13 @@ class Woops_Http_Cookie
     /**
      * Class constructor
      * 
+     * @param   string  The cookie's name
+     * @param   string  The cookie's value
+     * @param   int     The cookie's expiration date, as a timestamp
+     * @param   string  The cookie's path
+     * @param   string  The cookie's domain
+     * @param   boolean Wether the cookie is secured
+     * @param   boolean Wether the cookie is accessible only through the HTTP protocol
      * @return  void
      */
     public function __construct( $name, $value = '', $expires = 0, $path = '/', $domain = '', $secure = false, $httpOnly = false )
@@ -77,114 +84,166 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Returns a string representation of the HTTP cookie
      * 
+     * @return  string  The string representation of the HTTP cookie
      */
     public function __toString()
     {
+        // Name and value of the cookie
         $cookie = $this->_name . '=' . urlencode( $this->_value ) . ';';
         
+        // Checks for an expiration date
         if( $this->_expires ) {
             
+            // Adds the expiration date (as in RFC-2822)
             $cookie .= ' expires=' . date( 'r', $this->_expires ) . ';';
         }
         
+        // Checks for a path
         if( $this->_path ) {
             
+            // Adds the path
             $cookie .= ' path=' . $this->_path . ';';
         }
         
+        // Checks for a domain
         if( $this->_domain ) {
             
+            // Adds the domain
             $cookie .= ' domain=' . $this->_domain . ';';
         }
         
+        // Checks if the cookie is secure
         if( $this->_secure ) {
             
+            // Adds the secure option
             $cookie .= ' secure;';
         }
         
+        // Checks if the cookie is available only through the HTTP protocol
         if( $this->_httpOnly ) {
             
+            // Adds the HTTP only option
             $cookie .= ' HttpOnly;';
         }
         
+        // Returns the cookie string
         return $cookie;
     }
     
     /**
+     * Creates a cookie object from a string
      * 
+     * @param   string                      The cookie string
+     * @return  Woops_Http_Cookie           The cookie object
+     * @throws  Woops_Http_Cookie_Exception If the cookie string cannot be parsed
      */
     public static function createCookieObject( $str )
     {
+        // Finds the position of the first '=' character
         $equal = strpos( $str, '=' );
-    
+        
+        // Checks for the '=' character
         if( !$equal ) {
             
+            // Invalid cookie - No '=' character
             throw new Woops_Http_Cookie_Exception(
                 'Invalid cookie: \'' . $str . '\'',
                 Woops_Http_Cookie_Exception::EXCEPTION_BAD_COOKIE
             );
         }
         
+        // Gets the cookie's name
         $name    = trim( substr( $str, 0, $equal ) );
+        
+        // Gets the cookie options
         $options = trim( substr( $str, $equal + 1 ) );
+        
+        // Gets the cookie's options' parts
         $parts   = explode( ';', $options );
+        
+        // Gets the cookie valie
         $value   = trim( array_shift( $parts ) );
         
+        // Creates the cookie object
         $cookie  = new self( $name, $value );
         
+        // Process each part
         foreach( $parts as $part ) {
             
+            // Position of the '=' character
             $equal = strpos( $part, '=' );
             
+            // Checks for the '=' character
             if( !$equal ) {
                 
+                // Option without a value
                 $name = trim( $part );
                 
             } else {
                 
+                // Gets the name and the value of the option
                 $name  = trim( substr( $part, 0, $equal ) );
                 $value = trim( substr( $part, $equal + 1 ) );
             }
             
+            // Checks the option name
             switch( $name ) {
                 
+                // Expiration date
                 case 'expires';
                     
+                    // Sets the expiration date (as a timestamp)
                     $cookie->setExpires( strtotime( $value ) );
                     break;
                     
+                // Path
                 case 'path';
                     
+                    // Sets the cookie's path
                     $cookie->setPath( $value );
                     break;
                     
+                // Domain
                 case 'domain';
                     
+                    // Sets the cookie's domain
                     $cookie->setDomain( $value );
                     break;
                     
+                // Secure option
                 case 'secure';
                     
+                    // The cookie is secure
                     $cookie->setSecure( true );
                     break;
                     
+                // HTTP only option
                 case 'HttpOnly';
                     
+                    // The cookie is accessible only through the HTTP protocol
                     $cookie->setHttpOnly( true );
                     break;
                 
+                // Unknown option
                 default:
                     
                     break;
             }
         }
         
+        // Returns the cookie object
         return $cookie;
     }
     
     /**
+     * Sets the cookie
      * 
+     * This method will call the PHP setcookie() function, with all the
+     * parameters set on the current instance.
+     * 
+     * @return  boolean Wether the cookie has been set
      */
     public function setCookie()
     {
@@ -200,7 +259,19 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Gets the cookie's name
      * 
+     * @return  string  The cookie's name
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
+    
+    /**
+     * Gets the cookie's value
+     * 
+     * @return  string  The cookie's value
      */
     public function getValue()
     {
@@ -208,15 +279,20 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Gets the cookie's expiration date
      * 
+     * @param   boolean Wether to return a timestamp (false - by default), or a RFC-2822 date (true)
+     * @return  mixed   The cookie's expiration date
      */
-    public function getExpires()
+    public function getExpires( $asDate = false )
     {
-        return $this->_expires;
+        return ( $asDate ) ? date( 'r', $this->_expires ) : $this->_expires;
     }
     
     /**
+     * Gets the cookie's path
      * 
+     * @return  string  The cookie's path
      */
     public function getPath()
     {
@@ -224,7 +300,9 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Gets the cookie's domain
      * 
+     * @return  string  The cookie's domain
      */
     public function getDomain()
     {
@@ -232,7 +310,9 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Gets the cookie's secure option
      * 
+     * @return  boolean True if the cookie is secured, otherwise false
      */
     public function getSecure()
     {
@@ -240,7 +320,9 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Gets the cookie's HTTP only option
      * 
+     * @return  boolean True if the cookie is available only through the HTTP protocol, otherwise false
      */
     public function getHttpOnly()
     {
@@ -248,7 +330,10 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Sets the cookie's value
      * 
+     * @param   string  The cookie's value
+     * @return  void
      */
     public function setValue( $value )
     {
@@ -256,15 +341,22 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Sets the cookie's expiration date
      * 
+     * @param   mixed   The cookie's expiration date
+     * @param   boolean Wether the date is a timestamp (false - by default), or a RFC-2822 date (true)
+     * @return  void
      */
-    public function setExpires( $value )
+    public function setExpires( $value, $asDate = false )
     {
-        $this->_expires = ( int )$value;
+        $this->_expires = ( $asDate ) ? strtotime( $value ) : ( int )$value;
     }
     
     /**
+     * Sets the cookie's path
      * 
+     * @param   string  The cookie's path
+     * @return  void
      */
     public function setPath( $value )
     {
@@ -272,7 +364,10 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Sets the cookie's domain
      * 
+     * @param   string  The cookie's domain
+     * @return  void
      */
     public function setDomain( $value )
     {
@@ -280,7 +375,10 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Sets the cookie's secure option
      * 
+     * @param   boolean Wether the cookie is secure
+     * @return  void
      */
     public function setSecure( $value )
     {
@@ -288,7 +386,10 @@ class Woops_Http_Cookie
     }
     
     /**
+     * Sets the cookie's HTTP only option
      * 
+     * @param   boolean Wether the cookie is available only through the HTTP protocol
+     * @return  void
      */
     public function setHttpOnly( $value )
     {
