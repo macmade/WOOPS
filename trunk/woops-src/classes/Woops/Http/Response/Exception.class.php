066fa06ffe9c5e65eec91c1b20d3f519ba2b5296
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-21 18:57:41 +0000

    Added the exception codes for the HTTP response class.
    git-svn-id: https://woops.googlecode.com/svn@394 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Http/Response/Exception.class.php
+++ trunk/woops-src/classes/Woops/Http/Response/Exception.class.php
@@ -18,7 +18,7 @@
  * @version     1.0
  * @package     Woops.Http.Response
  */
-final class Woops_Http_Cookie_Response extends Woops_Core_Exception_Base
+final class Woops_Http_Response_Response extends Woops_Core_Exception_Base
 {
     /**
      * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
@@ -28,4 +28,9 @@ final class Woops_Http_Cookie_Response extends Woops_Core_Exception_Base
     /**
      * Error codes for the exceptions
      */
+    const EXCEPTION_INVALID_CODE            = 0x01;
+    const EXCEPTION_INVALID_HTTP_STATUS     = 0x02;
+    const EXCEPTION_INVALID_CHUNKED_CONTENT = 0x03;
+    const EXCEPTION_NO_GZUNCOMPRESS         = 0x04;
+    const EXCEPTION_NO_GZINFLATE            = 0x05;
 }
