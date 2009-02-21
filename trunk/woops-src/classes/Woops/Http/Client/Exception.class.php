8e27d2f421e690f19d0a5657a189f93f4b0de258
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-21 11:57:59 +0000

    Added the placeholders for the HTTP classes.
    git-svn-id: https://woops.googlecode.com/svn@379 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Http/Client/Exception.class.php
@@ -0,0 +1,31 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Exception class for the Woops_Http_Client class
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Http.Client
+ */
+final class Woops_Http_Client_Exception extends Woops_Core_Exception_Base
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * Error codes for the exceptions
+     */
+}
---
+++ trunk/woops-src/classes/Woops/Http/Cookie.class.php
@@ -0,0 +1,27 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# (c) 2009 eosgarden - Jean-David Gadina (macmade@eosgarden.com)               #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * HTTP cookie class
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Http
+ */
+class Woops_Http_Cookie
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+}
