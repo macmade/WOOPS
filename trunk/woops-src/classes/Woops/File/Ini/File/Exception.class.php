07debf14020a68ffa428fcc3676b7c9a85631cf4
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-02-28 19:12:17 +0000

    Added the exception class for the INI file class.
    git-svn-id: https://woops.googlecode.com/svn@469 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/File/Ini/File/Exception.class.php
@@ -0,0 +1,35 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Exception class for the Woops_File_Ini_File class
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.File.Ini.File
+ */
+final class Woops_File_Ini_File_Exception extends Woops_Core_Exception_Base
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * Error codes for the exceptions
+     */
+    const EXCEPTION_NO_DIR             = 0x01;
+    const EXCEPTION_DIR_NOT_WRITEABLE  = 0x02;
+    const EXCEPTION_FILE_NOT_WRITEABLE = 0x03;
+    const EXCEPTION_WRITE_ERROR        = 0x04;
+}
