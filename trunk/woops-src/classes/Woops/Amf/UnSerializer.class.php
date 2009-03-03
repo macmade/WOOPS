b9611576e28d73d4b345c53d8451adb4600f7b7f
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-03-03 16:37:15 +0000

    Added the placeholders for the AMF serializer and unserializer.
    git-svn-id: https://woops.googlecode.com/svn@551 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Amf/UnSerializer.class.php
@@ -0,0 +1,27 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * AMF un-serializer
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Amf
+ */
+class Woops_Amf_UnSerializer
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+}
