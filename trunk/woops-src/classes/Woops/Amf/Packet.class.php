95c3ca056531a883d8ee00857d1af15213318181
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-03-05 07:27:27 +0000

    Added the AMF packet abstract.
    git-svn-id: https://woops.googlecode.com/svn@578 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Amf/Packet.class.php
@@ -0,0 +1,27 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * Abstract class for the AMF packet classes
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Amf.Packet
+ */
+abstract class Woops_Amf_Packet
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+}
