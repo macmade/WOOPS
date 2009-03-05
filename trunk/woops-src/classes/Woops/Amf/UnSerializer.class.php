36d55bb51caf74422dd6329547813d595320b193
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-03-05 06:10:29 +0000

    Added the constructor, which initializes the AMF stream object.
    git-svn-id: https://woops.googlecode.com/svn@567 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Amf/UnSerializer.class.php
+++ trunk/woops-src/classes/Woops/Amf/UnSerializer.class.php
@@ -24,4 +24,42 @@ class Woops_Amf_UnSerializer
      * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
      */
     const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The binary stream
+     */
+    protected $_stream = NULL;
+    
+    /**
+     * The AMF packet object
+     */
+    protected $_packet = NULL;
+    
+    /**
+     * Class constructor
+     * 
+     * @param   string  The AMF packet raw data
+     * @return  void
+     */
+    public function __construct( $rawData )
+    {
+        // Creates the binary stream object
+        $this->_stream  = new Woops_Amf_Binary_Stream( $rawData );
+        
+        // Gets the packet version
+        $version        = $this->_stream->unsignedShort();
+        
+        // Creates a new AMF packet object
+        $this->_packet  = new Woops_Amf_Packet( $version );
+    }
+    
+    /**
+     * Gets the AMF packet object
+     * 
+     * @return  Woops_Amf_Packet    The AMF packet object
+     */
+    public function getPacket()
+    {
+        return $this->_packet;
+    }
 }
