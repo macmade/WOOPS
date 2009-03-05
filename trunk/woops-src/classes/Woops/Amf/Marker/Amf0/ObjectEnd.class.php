2e02c7c7c18b1d3d4fee1847bbf6c2fe79bd309a
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-03-05 07:11:18 +0000

    Added the placeholders for the AMF0 markers.
    git-svn-id: https://woops.googlecode.com/svn@574 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Amf/Marker/Amf0/ObjectEnd.class.php
@@ -0,0 +1,32 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * AMF0 object end marker (0x09)
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Amf.Marker.Amf0
+ */
+class Woops_Amf_Marker_Amf0_ObjectEnd extends Woops_Amf_Marker
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The AMF marker type
+     */
+    protected $_type = 0x09;
+}
---
+++ trunk/woops-src/classes/Woops/Amf/Marker/Amf0/String.class.php
@@ -0,0 +1,32 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * AMF0 string marker (0x02)
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Amf.Marker.Amf0
+ */
+class Woops_Amf_Marker_Amf0_String extends Woops_Amf_Marker
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The AMF marker type
+     */
+    protected $_type = 0x02;
+}
