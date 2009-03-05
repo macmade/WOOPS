36a7655f9f98fbfc1f43d64147e581e7fa9a81da
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-03-05 06:10:03 +0000

    Added the placeholders for the AMF message and AMF body.
    git-svn-id: https://woops.googlecode.com/svn@566 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Amf/Message.class.php
@@ -0,0 +1,27 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id$
+
+/**
+ * AMF message
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Amf
+ */
+class Woops_Amf_Message
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+}
