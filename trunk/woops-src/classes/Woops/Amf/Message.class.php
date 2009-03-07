7df650d08d07618cf7001789a1a3c8d2e25c99fe
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-03-07 18:17:54 +0000

    Added the methods for the AMF messages.
    git-svn-id: https://woops.googlecode.com/svn@594 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Amf/Message.class.php
+++ trunk/woops-src/classes/Woops/Amf/Message.class.php
@@ -44,4 +44,61 @@ class Woops_Amf_Message
      * The message's name
      */
     protected $_name       = '';
+    
+    /**
+     * Class constructor
+     * 
+     * @param   string              The message's name
+     * @param   string              The target URI
+     * @param   string              The request URI
+     * @param   Woops_Amf_Marker    The AMF marker object
+     * @return  void
+     */
+    public function __construct( $name, $targetUri, $requestUri, Woops_Amf_Marker $marker )
+    {
+        $this->_name           = ( string )$name;
+        $this->_marker         = $marker;
+        $this->_targetUri      = new Woops_Uniform_Resource_Identifier( $targetUri );
+        $this->_requestUri     = new Woops_Uniform_Resource_Identifier( $requestUri );
+    }
+    
+    /**
+     * Gets the message's name
+     * 
+     * @return  string  The message's name
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
+    
+    /**
+     * Gets the AMF marker
+     * 
+     * @return  Woops_Amf_Marker The AMF marker
+     */
+    public function getMarker()
+    {
+        return $this->_marker;
+    }
+    
+    /**
+     * Gets the target URI
+     * 
+     * @return  Woops_Uniform_Resource_Identifier   The target URI object
+     */
+    public function getTargetUri()
+    {
+        return $this->_targetUri;
+    }
+    
+    /**
+     * Gets the request URI
+     * 
+     * @return  Woops_Uniform_Resource_Identifier   The request URI object
+     */
+    public function getRequestUri()
+    {
+        return $this->_requestUri;
+    }
 }
