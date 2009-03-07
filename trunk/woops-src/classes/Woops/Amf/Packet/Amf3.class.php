8a6a9d3f5dab4c31fcfb8ab0a3f3da67d10d7171
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-03-07 18:18:50 +0000

    Added the AMF3 marker types.
    git-svn-id: https://woops.googlecode.com/svn@596 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Amf/Packet/Amf3.class.php
+++ trunk/woops-src/classes/Woops/Amf/Packet/Amf3.class.php
@@ -26,6 +26,23 @@ class Woops_Amf_Packet_Amf3 extends Woops_Amf_Packet
     const PHP_COMPATIBLE = '5.2.0';
     
     /**
+     * The allowed AMF markers
+     */
+    const MARKER_UNDEFINED  = 0x0000;
+    const MARKER_NULL       = 0x0001;
+    const MARKER_FALSE      = 0x0002;
+    const MARKER_TRUE       = 0x0003;
+    const MARKER_INTEGER    = 0x0004;
+    const MARKER_DOUBLE     = 0x0005;
+    const MARKER_STRING     = 0x0006;
+    const MARKER_XML_DOC    = 0x0007;
+    const MARKER_DATE       = 0x0008;
+    const MARKER_ARRAY      = 0x0009;
+    const MARKER_OBJECT     = 0x000A;
+    const MARKER_XML        = 0x000B;
+    const MARKER_BYTE_ARRAY = 0x000C;
+    
+    /**
      * The AMF packet version
      */
     protected $_version = 3;
@@ -33,19 +50,19 @@ class Woops_Amf_Packet_Amf3 extends Woops_Amf_Packet
     /**
      * The allowed AMF markers
      */
-    protected $_packets = array(
-        0x00 => 'Woops_Amf_Marker_Amf3_Undefined',
-        0x01 => 'Woops_Amf_Marker_Amf3_Null',
-        0x02 => 'Woops_Amf_Marker_Amf3_False',
-        0x03 => 'Woops_Amf_Marker_Amf3_True',
-        0x04 => 'Woops_Amf_Marker_Amf3_Integer',
-        0x05 => 'Woops_Amf_Marker_Amf3_Double',
-        0x06 => 'Woops_Amf_Marker_Amf3_String',
-        0x07 => 'Woops_Amf_Marker_Amf3_XmlDoc',
-        0x08 => 'Woops_Amf_Marker_Amf3_Date',
-        0x09 => 'Woops_Amf_Marker_Amf3_Array',
-        0x0A => 'Woops_Amf_Marker_Amf3_Object',
-        0x0B => 'Woops_Amf_Marker_Amf3_Xml',
-        0x0C => 'Woops_Amf_Marker_Amf3_ByteArray'
+    protected $_markers = array(
+        0x0000 => 'Woops_Amf_Marker_Amf3_Undefined',
+        0x0001 => 'Woops_Amf_Marker_Amf3_Null',
+        0x0002 => 'Woops_Amf_Marker_Amf3_False',
+        0x0003 => 'Woops_Amf_Marker_Amf3_True',
+        0x0004 => 'Woops_Amf_Marker_Amf3_Integer',
+        0x0005 => 'Woops_Amf_Marker_Amf3_Double',
+        0x0006 => 'Woops_Amf_Marker_Amf3_String',
+        0x0007 => 'Woops_Amf_Marker_Amf3_XmlDoc',
+        0x0008 => 'Woops_Amf_Marker_Amf3_Date',
+        0x0009 => 'Woops_Amf_Marker_Amf3_Array',
+        0x000A => 'Woops_Amf_Marker_Amf3_Object',
+        0x000B => 'Woops_Amf_Marker_Amf3_Xml',
+        0x000C => 'Woops_Amf_Marker_Amf3_ByteArray'
     );
 }
