b7bf62d89dffb10d9c91a8d02f1c709f57b1e3d4
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-03-09 03:02:18 +0000

    Added the __toString() method in the AMF marker classes.
    git-svn-id: https://woops.googlecode.com/svn@630 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Amf/Marker/Amf3/Date.class.php
+++ trunk/woops-src/classes/Woops/Amf/Marker/Amf3/Date.class.php
@@ -38,4 +38,12 @@ class Woops_Amf_Marker_Amf3_Date extends Woops_Amf_Marker_Amf3
      */
     public function processData( Woops_Amf_Binary_Stream $stream )
     {}
+    
+    /**
+     * Gets the AMF marker as binary
+     * 
+     * @return  string  The AMF marker
+     */
+    public function __toString()
+    {}
 }
