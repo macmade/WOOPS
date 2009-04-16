95e36af0351f13ec57af75bd9acf1124772fee23
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-04-16 15:10:54 +0000

    Added the SWF parser placeholder.
    git-svn-id: https://woops.googlecode.com/svn@653 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Swf/Parser.class.php
@@ -0,0 +1,79 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id: Parser.class.php 588 2009-03-07 11:52:36Z macmade $
+
+/**
+ * SWF file parser
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Swf
+ */
+class Woops_Swf_Parser
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * An instance of the Woops_Swf_File class
+     */
+    protected $_swfFile  = NULL;
+    
+    /**
+     * The binary stream
+     */
+    protected $_stream   = NULL;
+    
+    /**
+     * The file path
+     */
+    protected $_filePath = '';
+    
+    /**
+     * Class constructor
+     * 
+     * @param   string      The location of the SWF file
+     * @return  NULL
+     */
+    public function __construct( $file )
+    {
+        // Create a new SWF file object
+        $this->_swfFile = new Woops_Swf_File();
+        
+        // Stores the file path
+        $this->_filePath = $file;
+        
+        // Creates the binary stream
+        $this->_stream   = new Woops_Swf_Binary_File_Stream( $file );
+        
+        // Parses the file
+        $this->_parseFile();
+        
+        // Deletes the stream object
+        unset( $this->_stream );
+    }
+    
+    protected function _parseFile()
+    {}
+    
+    /**
+     * Gets the SWF file object
+     * 
+     * @return  Woops_Swf_File  The SWF file object
+     */
+    public function getSwfFile()
+    {
+        return $this->_swfFile;
+    }
+}
