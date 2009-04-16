97d7b46e73b8e2f9d615b0042b4583beea0f61f1
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-04-16 15:20:26 +0000

    Added the SWF file placeholder.
    git-svn-id: https://woops.googlecode.com/svn@654 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Swf/File.class.php
@@ -0,0 +1,59 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id: Parser.class.php 588 2009-03-07 11:52:36Z macmade $
+
+/**
+ * SWF file
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Swf
+ */
+class Woops_Swf_File
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The SWF header
+     */
+    protected $_header = NULL;
+    
+    /**
+     * The SWF tags
+     */
+    protected $_tags   = array();
+    
+    /**
+     * Class constructor
+     * 
+     * @param   int     The SWF version
+     * @return  NULL
+     */
+    public function __construct( $version = 10 )
+    {
+        // Creates a SWF header
+        $this->_header = new Woops_Swf_Header( $version );
+    }
+    
+    /**
+     * Gets the SWF header object
+     * 
+     * @return  Woops_Swf_Header    The header object
+     */
+    public function getHeader()
+    {
+        return $this->_header;
+    }
+}
