3fe559ea968ebbd9a0a3e8f5f8ca0861545378ba
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-04-18 01:05:18 +0000

    Added base processing for the DefineShape tag.
    git-svn-id: https://woops.googlecode.com/svn@690 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Swf/Tag/Define/Shape.class.php
+++ trunk/woops-src/classes/Woops/Swf/Tag/Define/Shape.class.php
@@ -12,8 +12,15 @@
 # $Id: Stream.class.php 637 2009-03-09 09:05:52Z macmade $
 
 /**
+ * SWF DefineShape tag
  * 
- *
+ * The DefineShape tag defines a shape for later use by control tags such as
+ * PlaceObject. The ShapeId uniquely identifies this shape as 'character' in the
+ * Dictionary. The ShapeBounds field is the rectangle that completely encloses
+ * the shape. The SHAPEWITHSTYLE structure includes all the paths, fill styles
+ * and line styles that make up the shape.
+ * The minimum file format version is SWF 1.
+ * 
  * @author      Jean-David Gadina <macmade@eosgarden.com>
  * @version     1.0
  * @package     Woops.Swf.Tag.Define.Shape
@@ -28,7 +35,31 @@ class Woops_Swf_Tag_Define_Shape extends Woops_Swf_Tag
     /**
      * The SWF tag type
      */
-    protected $_type = 0x02;
+    protected $_type        = 0x02;
+    
+    /**
+     * The ID for this character
+     */
+    protected $_shapeId     = 0;
+    
+    /**
+     * The bounds of the shape
+     */
+    protected $_shapeBounds = NULL;
+    
+    /**
+     * Class constructor
+     * 
+     * @param   Woops_Swf_File  The instance of the SWF file in which the tag is contained
+     */
+    public function __construct( Woops_Swf_File $file )
+    {
+        // Calls the parent constructor
+        parent::__construct( $file );
+        
+        // Creates a new rectangle record
+        $this->_shapeBounds = new Woops_Swf_Record_Rectangle();
+    }
     
     /**
      * Process the raw data from a binary stream
@@ -36,5 +67,42 @@ class Woops_Swf_Tag_Define_Shape extends Woops_Swf_Tag
      * @return  void
      */
     public function processData( Woops_Swf_Binary_Stream $stream )
-    {}
+    {
+        // Gets the shape ID
+        $this->_shapeId = $stream->littleEndianUnsignedShort();
+        
+        // Processes the rectangle data
+        $this->_shapeBounds->processData( $stream );
+    }
+    
+    /**
+     * Gets the shape bounds
+     * 
+     * @return  Woops_Swf_Record_Rectangle  The rectangle object for the shape bounds
+     */
+    public function getShapeBounds()
+    {
+        return $this->_shapeBounds;
+    }
+    
+    /**
+     * Gets the shape ID
+     * 
+     * @return  int     The shape ID
+     */
+    public function getShapeId()
+    {
+        return $this->_shapeId;
+    }
+    
+    /**
+     * Sets the shape ID
+     * 
+     * @param   int     The shape ID
+     * @return  void
+     */
+    public function setShapeId( $id )
+    {
+        $this->_shapeId = ( int )$id;
+    }
 }
