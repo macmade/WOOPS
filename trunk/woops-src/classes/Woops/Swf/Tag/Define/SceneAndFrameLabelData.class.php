bc920ce507c3a66a32043320fe927a0ce7feb5ca
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-04-18 16:10:59 +0000

    Data of the DefineSceneAndFrameLabelData tag is now processed.
    git-svn-id: https://woops.googlecode.com/svn@703 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Swf/Tag/Define/SceneAndFrameLabelData.class.php
+++ trunk/woops-src/classes/Woops/Swf/Tag/Define/SceneAndFrameLabelData.class.php
@@ -12,7 +12,11 @@
 # $Id: Stream.class.php 637 2009-03-09 09:05:52Z macmade $
 
 /**
+ * SWF DefineSceneAndFrameLabelData Tag
  * 
+ * The DefineSceneAndFrameLabelData tag contains scene and frame label data for
+ * a MovieClip. Scenes are supported for the main timeline only, for all other
+ * movie clips a single scene is exported.
  * 
  * @author      Jean-David Gadina <macmade@eosgarden.com>
  * @version     1.0
@@ -28,7 +32,22 @@ class Woops_Swf_Tag_Define_SceneAndFrameLabelData extends Woops_Swf_Tag
     /**
      * The SWF tag type
      */
-    protected $_type = 0x56;
+    protected $_type       = 0x56;
+    
+    /**
+     * The scenes
+     */
+    protected $_scenes     = array();
+    
+    /**
+     * The frame offset for the scenes
+     */
+    protected $_offsets    = array();
+    
+    /**
+     * The frame labels
+     */
+    protected $frameLabels = array();
     
     /**
      * Process the raw data from a binary stream
@@ -36,5 +55,31 @@ class Woops_Swf_Tag_Define_SceneAndFrameLabelData extends Woops_Swf_Tag
      * @return  void
      */
     public function processData( Woops_Swf_Binary_Stream $stream )
-    {}
+    {
+        // Resets the storage arrays
+        $this->_offsets    = array();
+        $this->_scenes     = array();
+        $this->frameLabels = array();
+        
+        // Gets the number of scenes
+        $sceneCount        = $stream->encodedU32();
+        
+        // Process eachs scene
+        for( $i = 0; $i < $sceneCount; $i++ ) {
+            
+            // Gets the frame offset and name for the current scene
+            $this->_offsets[] = $stream->encodedU32();
+            $this->_scenes[]  = $stream->nullTerminatedString();
+        }
+        
+        // Gets the number of frame labels
+        $frameLabelCount = $stream->encodedU32();
+        
+        // Process each frame label
+        for( $i = 0; $i < $frameLabelCount; $i++ ) {
+            
+            // Gets the frame number and label
+            $this->_frameLabels[ $stream->encodedU32() ] = $stream->nullTerminatedString();
+        }
+    }
 }
