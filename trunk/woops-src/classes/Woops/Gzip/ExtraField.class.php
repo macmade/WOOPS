0fcbb3e48a96f89ffa4a46cbb8825ce6a3711f03
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-04-19 18:03:58 +0000

    Added the abstract for the GZIP extra fields.
    git-svn-id: https://woops.googlecode.com/svn@742 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Gzip/ExtraField.class.php
@@ -0,0 +1,73 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id: Parser.class.php 588 2009-03-07 11:52:36Z macmade $
+
+/**
+ * Abstract for the GZIP extra fields
+ * 
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Gzip
+ */
+abstract class Woops_Gzip_ExtraField
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The extra field type
+     */
+    protected $_type = 0x0000;
+    
+    /**
+     * The data
+     */
+    protected $_data = '';
+    
+    /**
+     * Process the raw data from a binary stream
+     * 
+     * @param   Woops_Gzip_Binary_Stream    The binary stream
+     * @return  void
+     */
+    public function processData( Woops_Gzip_Binary_Stream $stream )
+    {
+        // Gets the data length
+        $length = $stream->littleEndianUnsignedShort();
+        
+        // Reads the field data
+        $this->_data = $stream->read( $length );
+    }
+    
+    /**
+     * Gets the field data
+     * 
+     * @return  string  The field data
+     */
+    public function getData()
+    {
+        return $this->_data;
+    }
+    
+    /**
+     * Sets the field data
+     * 
+     * @param   string  The field data
+     * @return  void
+     */
+    public function setData( $value )
+    {
+        $this->_data = ( string )$value;
+    }
+}
