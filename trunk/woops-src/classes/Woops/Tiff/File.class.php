5b060c598827eab34413723e84dbbdbe27c4824d
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-04-19 12:39:52 +0000

    The TIFF file class is now functionnal.
    git-svn-id: https://woops.googlecode.com/svn@705 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Tiff/File.class.php
+++ trunk/woops-src/classes/Woops/Tiff/File.class.php
@@ -24,4 +24,51 @@ class Woops_Tiff_File
      * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
      */
     const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The TIFF header
+     */
+    protected $_header = NULL;
+    
+    /**
+     * The image file directories (IFD)
+     */
+    protected $_ifds   = array();
+    
+    /**
+     * Class constructor
+     * 
+     * @return  void
+     */
+    public function __construct()
+    {
+        $this->_header = new Woops_Tiff_Header();
+    }
+    
+    /**
+     * Gets the TIFF header
+     * 
+     * @return  Woops_Tiff_Header   The TIFF header
+     */
+    public function getHeader()
+    {
+        return $this->_header;
+    }
+    
+    /**
+     * Creates a new IFD in the current TIFF file
+     * 
+     * @return  Woops_Tiff_Image_File_Directory The IFD object
+     */
+    public function newIfd()
+    {
+        // Creates the IFD
+        $ifd           = new Woops_Tiff_Image_File_Directory( $this );
+        
+        // Stores the IFD
+        $this->_ifds[] = $ifd;
+        
+        // Returns the IFD
+        return $ifd;
+    }
 }
