f6fceb4db6a14af84df9b0bd034b826b3142c9e9
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-04-20 16:51:10 +0000

    Added the ZIP extra field classes.
    git-svn-id: https://woops.googlecode.com/svn@754 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Zip/ExtraField/ZipIt/Macintosh/135.class.php
@@ -0,0 +1,32 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id: Parser.class.php 588 2009-03-07 11:52:36Z macmade $
+
+/**
+ * 
+ * 
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Zip.ExtraField.ZiptIt.Macintosh
+ */
+class Woops_Zip_ExtraField_ZipIt_Macintosh_135 extends Woops_Zip_ExtraField
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The type of the extra field
+     */
+    protected $_type = 0x2705;
+}
