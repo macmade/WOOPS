070ad4c91eddfd77a64f91f7b2381009c5e1c899
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-05-04 15:40:46 +0000

    Added a method for the 24bits unsigned integers.
    git-svn-id: https://woops.googlecode.com/svn@783 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Flv/Binary/Stream.class.php
+++ trunk/woops-src/classes/Woops/Flv/Binary/Stream.class.php
@@ -24,4 +24,20 @@ class Woops_Flv_Binary_Stream extends Woops_Binary_Stream
      * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
      */
     const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * Gets an unsigned 24bits integer from the stream
+     * 
+     * @return  int     The unsigned 24bits integer
+     */
+    public function u24Int()
+    {
+        // Read 3 bytes
+        $byte1 = $this->unsignedChar();
+        $byte2 = $this->unsignedChar();
+        $byte3 = $this->unsignedChar();
+        
+        // Returns the integer
+        return ( $byte1 << 16 ) | ( $byte2 << 8 ) | $byte3;
+    }
 }
