bb6f132d4ecfe5c8a2a7f866a071f822359d6592
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-05-04 16:05:57 +0000

    Added the FLV tag classes.
    git-svn-id: https://woops.googlecode.com/svn@787 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Flv/Tag/Script/Data.class.php
@@ -0,0 +1,45 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id: Parser.class.php 588 2009-03-07 11:52:36Z macmade $
+
+/**
+ * 
+ * 
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Flv.Tag.Script
+ */
+class Woops_Flv_Tag_Script_Data extends Woops_Flv_Tag
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The FLV tag type
+     */
+    protected $_type = 0x12;
+    
+    /**
+     * Process the raw data from a binary stream
+     * 
+     * @return  void
+     */
+    public function processData( Woops_Flv_Binary_Stream $stream )
+    {
+        // Calls the parent method
+        parent::processData( $stream );
+        
+        $stream->seek( $this->_dataSize, Woops_Flv_Binary_Stream::SEEK_CUR );
+    }
+}
