170429db2e7c56978f81cfdb005e61b37f5d616f
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-05-04 16:04:14 +0000

    The FLV file class is now functionnal.
    git-svn-id: https://woops.googlecode.com/svn@784 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/classes/Woops/Flv/File.class.php
+++ trunk/woops-src/classes/Woops/Flv/File.class.php
@@ -26,14 +26,30 @@ class Woops_Flv_File
     const PHP_COMPATIBLE = '5.2.0';
     
     /**
+     * The FLV tag types
+     */
+    const TAG_AUDIO  = 0x08;
+    const TAG_VIDEO  = 0x09;
+    const TAG_SCRIPT = 0x12;
+    
+    /**
+     * The FLV tag types with their corresponding PHP classname
+     */
+    protected static $_types = array(
+        0x08 => 'Woops_Flv_Tag_Audio_Data',
+        0x09 => 'Woops_Flv_Tag_Video_Data',
+        0x12 => 'Woops_Flv_Tag_Script_Data'
+    );
+    
+    /**
      * The FLV header
      */
-    protected $_header = NULL;
+    protected $_header       = NULL;
     
     /**
      * The FLV tags
      */
-    protected $_tags   = array();
+    protected $_tags         = array();
     
     /**
      * Class constructor
@@ -53,5 +69,42 @@ class Woops_Flv_File
     public function getHeader()
     {
         return $this->_header;
+    }
+    
+    /**
+     * Creates a new tag
+     * 
+     * @param   int                         The tag type (one of the TAG_XXX constant)
+     * @return  Woops_Flv_Tag               The tag object
+     * @throws  Woops_Flv_File_Exception    If the tag type is invalid
+     * @throws  Woops_Flv_File_Exception    If the tag type is audio, and if there is already an audio tag
+     * @throws  Woops_Flv_File_Exception    If the tag type is video, and if there is already a video tag
+     */
+    public function newTag( $type )
+    {
+        // Ensures we have an integer
+        $type = ( int )$type;
+        
+        // Checks for a valid tag type
+        if( !isset( self::$_types[ $type ] ) ) {
+            
+            // Error - Invalid tag type
+            throw new Woops_Flv_File_Exception(
+                'Invalid FLV tag type (' . $type . ')',
+                Woops_Flv_File_Exception::EXCEPTION_INVALID_TAG_TYPE
+            );
+        }
+        
+        // Gets the tag classname
+        $tagClass      = self::$_types[ $type ];
+        
+        // Creates the tag
+        $tag           = new $tagClass();
+        
+        // Stores the tag object
+        $this->_tags[] = $tag;
+        
+        // Returns the tag object
+        return $tag;
     }
 }
