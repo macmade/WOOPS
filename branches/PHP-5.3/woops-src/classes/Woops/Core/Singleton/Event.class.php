c3c0e9f04fc7c549501e3760f7f48c898b390e9d
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-05-10 04:43:03 +0000

    Added an abstract for the singleton classes.
    git-svn-id: https://woops.googlecode.com/svn@841 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ branches/PHP-5.3/woops-src/classes/Woops/Core/Singleton/Event.class.php
@@ -0,0 +1,38 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id: Helper.class.php 534 2009-03-03 07:15:08Z macmade $
+
+// File encoding
+declare( ENCODING = 'UTF-8' );
+
+// Internal namespace
+namespace Woops\Core\Singleton;
+
+/**
+ * Event object for the Woops\Core\Singleton\Base class
+ * 
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Core.Singleton
+ */
+class Event extends \Woops\Core\Event
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The available events
+     */
+    const EVENT_CONSTRUCT = 0x01;
+}
