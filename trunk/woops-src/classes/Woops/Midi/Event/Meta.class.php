9ad3326b9ba65abcf929dd3b31f36ffded5457c5
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-05-12 02:45:17 +0000

    Initial import of the Midi package.
    git-svn-id: https://woops.googlecode.com/svn@882 8673a9e1-e74b-d5cd-0c81-aacadf88860f

---
+++ trunk/woops-src/classes/Woops/Midi/Event/Meta.class.php
@@ -0,0 +1,50 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id: Parser.class.php 588 2009-03-07 11:52:36Z macmade $
+
+// File encoding
+declare( ENCODING = 'UTF-8' );
+
+// Internal namespace
+namespace Woops\Midi\Event;
+
+/**
+ * 
+ * 
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Midi.Event
+ */
+class Meta extends \Woops\Midi\Event
+{
+    /**
+     * The minimum version of PHP required to run this class (checked by the WOOPS class manager)
+     */
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
+     * The chunk type
+     */
+    protected $_type = 0xFF;
+    
+    /**
+     * Process the raw data from a binary stream
+     * 
+     * @return  void
+     */
+    public function processData( \Woops\Midi\Binary\Stream $stream )
+    {
+        $type   = $stream->unsignedChar();
+        $length = $stream->variableLengthQuantity();
+        $data   = $stream->read( $length );
+    }
+}
---
+++ trunk/woops-src/classes/Woops/Midi/Track/Event/Exception.class.php
@@ -0,0 +1,33 @@
+<?php
+################################################################################
+#                                                                              #
+#                WOOPS - Web Object Oriented Programming System                #
+#                                                                              #
+#                               COPYRIGHT NOTICE                               #
+#                                                                              #
+# Copyright (C) 2009 Jean-David Gadina (macmade@eosgarden.com)                 #
+# All rights reserved                                                          #
+################################################################################
+
+# $Id: Exception.class.php 559 2009-03-04 17:18:24Z macmade $
+
+// File encoding
+declare( ENCODING = 'UTF-8' );
+
+// Internal namespace
+namespace Woops\Midi\Track\Event;
+
+/**
+ * Exception class for the Woops\Midi\Track\Event class
+ * 
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     Woops.Midi.Track.Event
+ */
+final class Exception extends \Woops\Core\Exception\Base
+{
+    /**
+     * Error codes for the exceptions
+     */
+    const EXCEPTION_INVALID_EVENT_TYPE = 0x01;
+}
