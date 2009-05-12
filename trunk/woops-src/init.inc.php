5b763961999a3bdd676115711294a6d6300a5a41
macmade <macmade@8673a9e1-e74b-d5cd-0c81-aacadf88860f>
2009-05-12 02:27:49 +0000

    Better error reporting for older PHP versions.
    git-svn-id: https://woops.googlecode.com/svn@879 8673a9e1-e74b-d5cd-0c81-aacadf88860f

--- trunk/woops-src/init.inc.php
+++ trunk/woops-src/init.inc.php
@@ -11,31 +11,43 @@
 
 # $Id$
 
-// Sets the error reporting level to the highest possible value
-error_reporting( E_ALL | E_STRICT );
+// Checks if we are running PHP 5
+if( ( double )PHP_VERSION >= 5 ) {
+    
+    // Sets the error reporting level to the highest possible value (PHP 5)
+    error_reporting( E_ALL | E_STRICT );
+    
+    // Sets a dummy timezone, to prevent warnings if an error occurs before the configured timezone is set
+    date_default_timezone_set( 'Europe/Zurich' );
+    
+} else {
+    
+    // Sets the error reporting level to the highest possible value (PHP 4)
+    error_reporting( E_ALL );
+}
 
-// Checks the PHP version
-if( ( double )PHP_VERSION < 5.3 ) {
+// Checks the PHP version required to use WOOPS
+if( version_compare( PHP_VERSION, '5.3.0RC2', '<' ) ) {
     
     // We are not running PHP 5.3 or greater
     trigger_error(
-        'PHP version 5.3 is required to use this script (actual version is ' . PHP_VERSION . ')',
+        'PHP version 5.3.0RC2 is required to use WOOPS (actual version is ' . PHP_VERSION . ')',
         E_USER_ERROR
     );
+    
+    // Aborts the script
+    exit();
 }
 
 // File encoding
 declare( ENCODING = 'UTF-8' );
-
-// Sets a dummy timezone, to prevent warnings if an error occurs before the configured timezone is set
-date_default_timezone_set( 'Europe/Zurich' );
 
 // Checks for the SPL
 if( !function_exists( 'spl_autoload_register' ) ) {
     
     // The SPL is unavailable
     throw new Exception(
-        'The SPL (Standard PHP Library) is required to use this script'
+        'The SPL (Standard PHP Library) is required to use WOOPS'
     );
 }
 
@@ -44,11 +56,11 @@ if( !class_exists( 'SimpleXMLElement' ) ) {
     
     // SimpleXMLElement is unavailable
     throw new Exception(
-        'The SimpleXMLElement class is required to use this script'
+        'The SimpleXMLElement class is required to use WOOPS'
     );
 }
 
-// Includes the Woops class manager
+// Includes the WOOPS class manager
 require_once(
     __DIR__
   . DIRECTORY_SEPARATOR
@@ -61,7 +73,7 @@ require_once(
   . 'ClassManager.class.php'
 );
 
-// Registers an SPL autoload method to use to load the classes form the Woops project
+// Registers an SPL autoload method to use to load the classes form the WOOPS project
 spl_autoload_register( array( 'Woops\Core\ClassManager', 'autoLoad' ) );
 
 // Sets the error and exception handlers - From now every mistake will produce a fatal error
@@ -71,5 +83,5 @@ set_error_handler(     array( 'Woops\Core\Error\Handler',     'handleError' ) );
 // Sets the default timezone
 date_default_timezone_set( Woops\Core\Config\Getter::getInstance()->getVar( 'time', 'timezone' ) );
 
-// Loads the active modules
+// Initializes the active (loaded) modules
 Woops\Core\Module\Manager::getInstance()->initModules();
